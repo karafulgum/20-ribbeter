bafa146cabd235d8b5deef53b090af7f2252479a
Kara Fulgum <karafulgum@gmail.com>
2016-11-09 15:46:07 -0600

    Store user when register is posted

--- app/Http/Controllers/UserController.js
+++ app/Http/Controllers/UserController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const User = use('App/Model/User');
+const Hash = use('Hash');
 
 class UserController {
   * create(request, response) {
@@ -8,9 +9,31 @@ class UserController {
   }
 
   * store(request, response) {
+    const { username, email, password } = request.all();
 
+    try {
+      const user = yield User.create({
+        username,
+        email,
+        password: yield Hash.make(password),
+      });
+
+      yield request.auth.login(user);
+
+      yield request.with({
+        success: 'You have successfully created a new account',
+      }).flash();
+
+      response.redirect('/users');
+    } catch (e) {
+      yield request
+        .withOut('password')
+        .andWith({ error: 'That email is already taken' })
+        .flash();
+
+      response.redirect('back');
+    }
   }
-
 }
 
 module.exports = UserController;
