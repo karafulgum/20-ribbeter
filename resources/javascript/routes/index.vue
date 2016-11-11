f26a17d5aacf7fb008375b62dd7aaffc471f6c28
Kara Fulgum <karafulgum@gmail.com>
2016-11-11 13:27:56 -0600

    Now I did HTML Index Vue, the commit above is the landing page

--- resources/javascript/routes/index.vue
+++ resources/javascript/routes/index.vue
@@ -6,9 +6,9 @@
         <div class="card">
           <p class="card__title">New Ribbit</p>
           <div class="card__content">
-            <form class="card__form">
-              <label for="ribbit-post">What's Going On?</label>
-              <textarea rows="4"></textarea>
+            <form>
+              <label class="card__header" for="ribbit-post">What's Going On?</label>
+              <textarea rows="8" class="card__input" id="ribbit-post"></textarea>
             </form>
         </div>
         <div class="card-btn">
@@ -16,9 +16,19 @@
           <a href="#" class="card-btn__submit">Save</a>
         </div>
       </div>
+    </div>
 
       <div class="ribbit-grid__item ribbit-grid__item--main">
-
+        <div class="card">
+          <p class="card__title">See What's Happening</p>
+          <div class="card__content--btn">
+            <a href="#" class="card-btn__back card-btn__back--full">Load New Ribbits</a>
+          </div>
+          <div class="card__row">
+            <h3 class="card__username">$karafulgum</h3>
+            <p class="card__text">What the user inputted goes here</p>
+          </div>
+        </div>
       </div>
     </div>
 
